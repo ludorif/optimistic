89c7c61d75af61b28f9059feee2839aec2459eac
Ludorif <ludorif@test.com>
2025-08-07 14:32:12 +0100

    first files

---
+++ Main.py
@@ -0,0 +1,20 @@
+﻿import os
+from google import genai
+
+os.environ["GEMINI_API_KEY"] = "AIzaSyDCmmQEnq-A1RdP-Nx4BqyCmKgl87_KHXI"
+
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/news/<input>")
+def news(input):
+    client = genai.Client()
+
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=f"Imagine a newspaper's overly optimistic short article based on this event: \"{input}\".Propose only 1 choice"
+    )
+    return f"<p>{response.text}</p>"
---
+++ db.py
@@ -0,0 +1,23 @@
+﻿from flask import Flask
+from pymongo import MongoClient
+
+uri = "mongodb+srv://testuser:hgGWCIOcm1z7X9zM@cluster0.zrojvuw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+# Create a new client and connect to the server
+myclient = MongoClient(uri)
+
+# Send a ping to confirm a successful connection
+try:
+    mydb = myclient["optimistic"]
+
+    mycol = mydb["test"]
+
+    mydict = {"name": "John2", "address2": "Highway 372"}
+
+    x = mycol.insert_one(mydict)
+
+    print(mycol.find_one())
+
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
