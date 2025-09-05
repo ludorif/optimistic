35dfbfa1d9619d1b0ba6907f9d5e58504866096d
Ludorif <ludorif@test.com>
2025-09-05 10:02:44 +0100

    * css update for winners page

--- Frontend/src/components/OpEvent.jsx
+++ Frontend/src/components/OpEvent.jsx
@@ -4,21 +4,43 @@ import styled from "styled-components";
 import Grid from "@mui/material/Grid";
 
 const Image = styled.img`
-    width: 300px;
-    height: 300px;
-    border-radius: 50%;
-    overflow: hidden;
+    width: 75%;          /* Takes full width of parent */
+    aspect-ratio: 1 / 1;  /* Keeps it square */
+    border-radius: 50%;   /* Circle shape */
+    object-fit: cover;
+`;
+
+
+
+const Title = styled.h2`
+    
+    font-size: clamp(1rem, 2vw, 3rem);
+    background: linear-gradient(90deg, #000000, #0000ff);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    height: 25px;
+
+    border-top: 2px solid #333;
+    border-bottom: 2px solid #333;
+    padding: 10px 0;
+    margin: 20px 0;
+`;
+
+const Content = styled.p`
+    font-size: clamp(0.1rem, 1.5vw, 1rem);
+    animation: fadeIn 2s ease-in-out;
+    height: 100px;
 `;
 
 
 export default function  OpEvent ({event}) {
     return (
-        <>
-            <div style={{ height:200}}>
-                <h2>{event.title}</h2>
-                <p>{event.content} </p>
+        <div className="newspaper" >
+            <div >
+                <Title>{event.title}</Title>
+                <Content>{event.content} </Content>
             </div>
             <Image src={`https://images.pexels.com/photos/${event.photoId}/pexels-photo-${event.photoId}.jpeg`}/>
-        </>
+        </div>
     )
 }
