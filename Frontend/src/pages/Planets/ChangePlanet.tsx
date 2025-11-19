04b7007b5ca661c9d2998a4ea891f12f925d7563
Ludorif <ludorif@test.com>
2025-11-19 19:11:27 +0000

    * move to typescript

---
+++ Frontend/src/pages/Planets/ChangePlanet.tsx
@@ -0,0 +1,16 @@
+﻿/*
+ * Copyright (c) 2025 Ludovic Riffiod
+ */
+
+import Button from "@mui/material/Button";
+import React from "react";
+
+interface ChangePlanetProps {
+    OnClickFunction: () => void;
+}
+
+const ChangePlanet: React.FC<ChangePlanetProps> = ({OnClickFunction}) => {
+    return <Button onClick={OnClickFunction} variant="contained">ChangePlanet</Button>
+}
+
+export default ChangePlanet;
---
+++ Frontend/src/pages/Planets/NewPlanet.tsx
@@ -0,0 +1,56 @@
+﻿/*
+ * Copyright (c) 2025 Ludovic Riffiod
+ */
+
+import React, {useRef} from "react";
+import ExecuteRequest from "../../AxiosManager";
+import axios from "axios";
+import titleStyle from "../../Helper";
+import {TextField, ToggleButtonGroup} from "@mui/material";
+import {ToggablePlanet} from "../../components/Planet";
+import mars_surface from "../../assets/mars_surface.png";
+import lunar_surface from "../../assets/lunar_surface.jpg";
+import earth_surface from "../../assets/earth_surface.jpg";
+import Button from "@mui/material/Button";
+
+const CreateNewPlanet = () => {
+    const [planetType, setPlanetType] = React.useState('moon');
+    const planetNameRef = useRef<HTMLInputElement>(null);
+    const planetStoryRef = useRef<HTMLInputElement>(null);
+
+
+    const handlePlanetType = (event, newPlanetType) => {
+        setPlanetType(newPlanetType);
+    };
+
+    function CheckValues() {
+
+        ExecuteRequest(axios.post('planets/', {name: planetNameRef.current!.value, type: planetType}));
+
+    }
+
+
+    return <>
+
+        <h1 style={titleStyle}>Create your planet</h1>
+        <TextField inputRef={planetNameRef} label="Planet name" variant="outlined"/>
+        <p> Choose your planet style:</p>
+
+        <ToggleButtonGroup
+            value={planetType}
+            exclusive
+            onChange={handlePlanetType}
+            aria-label="text alignment"
+        >
+            <ToggablePlanet value="mars" surfaceImage={mars_surface}></ToggablePlanet>
+            <ToggablePlanet value="moon" surfaceImage={lunar_surface}></ToggablePlanet>
+            <ToggablePlanet value="earth" surfaceImage={earth_surface}></ToggablePlanet>
+
+        </ToggleButtonGroup>
+
+        <TextField inputRef={planetStoryRef} label="Starting story" variant="outlined"/>
+        <Button onClick={CheckValues} variant="contained">Confirm creation</Button>
+    </>
+}
+
+export default CreateNewPlanet;
