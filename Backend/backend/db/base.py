a6fc600c2a35b9ae71153af5d254c5deba023104
Ludorif <ludorif@test.com>
2026-03-26 17:10:51 +0000

    * make sqlite database file persistent in Render

--- Backend/backend/db/base.py
+++ Backend/backend/db/base.py
@@ -1,12 +1,14 @@
 #  Copyright (c) 2025 Ludovic Riffiod
 #
+import os
+
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
 Base = declarative_base()
 engine = create_engine(
-    "sqlite:///optimistic_db.db",
+    os.environ["SQLITE_DB_PATH"],
     connect_args={"check_same_thread": False},
     pool_size=5,
     max_overflow=0
